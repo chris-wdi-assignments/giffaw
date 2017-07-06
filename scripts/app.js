8b7a8476cffed8b9db154c7ea04e0ad066e6b4b7
Christopher Fujino <christopherfujino@gmail.com>
2017-07-06 15:51:23 -0700

    added: ajax request; loads images into DOM, but are broken

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,20 @@
-$(document).on("ready", function(){
-
+$(document).on("ready", function () {
+  $('form').on('submit', function (e) {
+    e.preventDefault();
+    let subject = $('#query').val();
+    let apiKey = $('#apiKey').val();
+    $.ajax({
+      method: "GET",
+      url: "https://api.giphy.com/v1/gifs/search",
+      data: $(this).serialize(),
+      success: getGifs
+    });
+  });
 });
+
+function getGifs (res) {
+  console.log(res);
+  res.data.forEach(function (gif) {
+    $('<img>').attr('src', gif.url).appendTo('.gif-gallery');
+  })
+}
